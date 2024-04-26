2da366fdb539f60a6daf9f7704405e05ba0a0734
Musa-Mabasa <musa14mabasa@gmail.com>
2024-04-26 09:09:15 +0200

    💄 Added the product card UI

--- src/app/components/admin-products/admin-products.component.ts
+++ src/app/components/admin-products/admin-products.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { matPlus, matSort } from '@ng-icons/material-icons/baseline';
+import { matCheck, matPlus, matSort } from '@ng-icons/material-icons/baseline';
+import { ProductCardComponent } from "../product-card/product-card.component";
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-admin-products',
-  standalone: true,
-  imports: [NgIconComponent],
-  templateUrl: './admin-products.component.html',
-  styleUrl: './admin-products.component.scss',
-  viewProviders: [
-    provideIcons({ matPlus, matSort}),
-  ],
+    selector: 'app-admin-products',
+    standalone: true,
+    templateUrl: './admin-products.component.html',
+    styleUrl: './admin-products.component.scss',
+    viewProviders: [
+        provideIcons({ matPlus, matSort, matCheck }),
+    ],
+    imports: [NgIconComponent, ProductCardComponent, NgIf]
 })
 export class AdminProductsComponent {
   tags = ["All products","Shoes", "Sport", "Kitchen", "Technology", "Gaming", "Clothes"]
   currentTag = 'All products'
-
+  sortBy = "price"
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,21 @@ export default {
       red: "red",
       "sidebar-item": "rgb(255,255,255, 0.3)",
       "search-color": "#f5f7fa",
-      black: "black"
+      black: "black",
     },
     extend: {
       fontFamily: {
         rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateColumns: {
+        sm: "repeat(2, minmax(0, 18rem))",
+        lg: "repeat(3, minmax(0, 18rem))",
+        xl: "repeat(4, minmax(0, 18rem))",
+        "2xl": "repeat(5, minmax(0, 18rem))",
+        "3xl": "repeat(5, minmax(0, 18rem))",
+      },
+      gridTemplateRows: {
+        main: "72px 1fr"
       },
     },
   },
