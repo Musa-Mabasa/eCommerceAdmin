b6589d0592dbd6a0d59ffe7c6b3b8863a44e01f0
Musa-Mabasa <musa14mabasa@gmail.com>
2024-04-26 13:45:09 +0200

    💄 Completed the edit page UI

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { matCheck } from '@ng-icons/material-icons/baseline';
+import { NgIf } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import {
+  matCheck,
+  matEdit,
+  matKeyboardArrowDown,
+  matPlus,
+} from "@ng-icons/material-icons/baseline";
 
 @Component({
-  selector: 'app-edit-product',
+  selector: "app-edit-product",
   standalone: true,
-  imports: [NgIconComponent],
-  templateUrl: './edit-product.component.html',
-  styleUrl: './edit-product.component.scss',
-  viewProviders: [provideIcons({ matCheck })],
+  imports: [NgIconComponent, NgIf],
+  templateUrl: "./edit-product.component.html",
+  styleUrl: "./edit-product.component.scss",
+  viewProviders: [
+    provideIcons({ matPlus, matCheck, matKeyboardArrowDown, matEdit }),
+  ],
 })
 export class EditProductComponent {
-
+  currency = "ZAR";
+  category = "Tech";
+  router = inject(Router);
 }
