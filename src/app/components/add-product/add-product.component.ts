9b78b8cd628dbe12b9974357fd14b2f4f5a1ad6f
Musa-Mabasa <musa14mabasa@gmail.com>
2024-04-28 11:53:58 +0200

    🐛 Restored the drag and drop upload

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -24,35 +24,4 @@ export class AddProductComponent {
   router = inject(Router);
   currency = "ZAR";
   category = "Tech";
-  imgView: HTMLElement | null;
-  chooseFile: HTMLInputElement | null;
-
-  constructor() {
-    this.imgView = document.getElementById("viewContainer");
-    this.chooseFile = document.getElementById("dropzone-file") as HTMLInputElement;
-    console.log(this.chooseFile);
-    
-
-    if (this.chooseFile) {
-      this.chooseFile.addEventListener("change", () => {
-        this.getImageData();
-      });
-    }
-  }
-
-  getImageData() {
-    if (!this.chooseFile) console.error("Element Error");
-
-    const files = this.chooseFile?.files?.[0];
-    console.log(files);
-    
-    if (files) {
-      const fileReader = new FileReader();
-      const result = fileReader.readAsDataURL(files);
-      fileReader.addEventListener("load", () => {
-        if (this.imgView)
-          this.imgView.innerHTML = '<img src="' + result + '" />';
-      });
-    }
-  }
 }
