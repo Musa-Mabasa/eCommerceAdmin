35d431fb60754a88210a0740da47cb613d6d9772
Musa-Mabasa <musa14mabasa@gmail.com>
2024-04-28 13:30:34 +0200

    💄 Added image edit UI

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -4,7 +4,9 @@ import { Router } from "@angular/router";
 import { NgIconComponent, provideIcons } from "@ng-icons/core";
 import {
   matCheck,
+  matClose,
   matEdit,
+  matImage,
   matKeyboardArrowDown,
   matPlus,
 } from "@ng-icons/material-icons/baseline";
@@ -16,11 +18,31 @@ import {
   templateUrl: "./edit-product.component.html",
   styleUrl: "./edit-product.component.scss",
   viewProviders: [
-    provideIcons({ matPlus, matCheck, matKeyboardArrowDown, matEdit }),
+    provideIcons({
+      matPlus,
+      matCheck,
+      matKeyboardArrowDown,
+      matEdit,
+      matImage,
+      matClose,
+    }),
   ],
 })
 export class EditProductComponent {
   currency = "ZAR";
   category = "Tech";
   router = inject(Router);
+  onImageEdit = false;
+  selectedFile?: {
+    name: string;
+    size: number;
+    type: string;
+    lastModified: number;
+    lastModifiedDate: Date;
+  };
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
+  }
 }
