b178743bc96203dd95fb5815293b2a0ae7ad42fa
Musa-Mabasa <musa14mabasa@gmail.com>
2024-04-28 12:58:40 +0200

    💄 Added file upload UI

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -8,6 +8,8 @@ import {
   matCheck,
   matKeyboardArrowDown,
   matEdit,
+  matImage,
+  matClose,
 } from "@ng-icons/material-icons/baseline";
 
 @Component({
@@ -17,11 +19,30 @@ import {
   templateUrl: "./add-product.component.html",
   styleUrl: "./add-product.component.scss",
   viewProviders: [
-    provideIcons({ matPlus, matCheck, matKeyboardArrowDown, matEdit }),
+    provideIcons({
+      matPlus,
+      matCheck,
+      matKeyboardArrowDown,
+      matEdit,
+      matImage,
+      matClose,
+    }),
   ],
 })
 export class AddProductComponent {
   router = inject(Router);
   currency = "ZAR";
   category = "Tech";
+  selectedFile?: {
+    name: string;
+    size: number;
+    type: string;
+    lastModified: number;
+    lastModifiedDate: Date;
+  };
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
+  }
 }
