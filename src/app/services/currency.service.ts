a595005e9e8e97d171cbcc5fb19593923142881a
Musa-Mabasa <musa14mabasa@gmail.com>
2024-05-06 10:07:36 +0200

    ♻️ place api key in env

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { CurrencyExhangeRates } from "../models/admin";
+import { currencyAPI_KEY } from "../../../environment/environment";
 
 @Injectable({
   providedIn: "root",
@@ -10,7 +11,7 @@ export class CurrencyService {
 
   getCurrencyExchangeRates(userCurrency: string) {
     return this.currencyAPi.get<CurrencyExhangeRates>(
-      `https://api.currencyapi.com/v3/latest?apikey=cur_live_QntszarIE7kEs00Ebmm7V8rEeyVNYfYmDNC8lbb7&currencies=EUR%2CUSD%2CGBP%2CZAR&base_currency=${userCurrency}`
+      `https://api.currencyapi.com/v3/latest?apikey=${currencyAPI_KEY}&currencies=EUR%2CUSD%2CGBP%2CZAR&base_currency=${userCurrency}`
     );
   }
 }
