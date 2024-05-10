c6607050af7a33c29c880424e95e763401e18b03
Musa-Mabasa <musa14mabasa@gmail.com>
2024-05-10 15:00:01 +0200

    🔥 Removed unused imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,5 @@
 import { Routes } from "@angular/router";
-import { LoginComponent } from "./components/login/login.component";
-import { SignInComponent } from "./components/sign-in/sign-in.component";
-import { HomeComponent } from "./components/home/home.component";
-import { RouteErrorComponent } from "./components/route-error/route-error.component";
 import { authGuard } from "./auth.guard";
-import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
-import { AdminProductsComponent } from "./components/admin-products/admin-products.component";
-import { AllProductsComponent } from "./components/all-products/all-products.component";
-import { EditProductComponent } from "./components/edit-product/edit-product.component";
-import { AddProductComponent } from "./components/add-product/add-product.component";
-import { PreviewProductComponent } from "./components/preview-product/preview-product.component";
 
 export const routes: Routes = [
   {
