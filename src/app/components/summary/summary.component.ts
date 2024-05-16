9d89fdf76496b6c72bed185886858be010dd9f1a
Musa-Mabasa <musa14mabasa@gmail.com>
2024-05-16 09:51:18 +0200

    💄 Added the count up animation

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from "@angular/core";
 import { NgIconComponent, provideIcons } from "@ng-icons/core";
-import { matArrowDropUp, matArrowOutward } from "@ng-icons/material-icons/baseline";
+import {
+  matArrowDropUp,
+  matArrowOutward,
+} from "@ng-icons/material-icons/baseline";
+import { CountUpDirective } from "../../directives/count-up.directive";
 
 @Component({
   selector: "app-summary",
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, CountUpDirective],
   templateUrl: "./summary.component.html",
   styleUrl: "./summary.component.scss",
   viewProviders: [provideIcons({ matArrowOutward, matArrowDropUp })],
